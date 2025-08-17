ae5f09fafa320582a68a63fd1f037791fda82790
Yevhenii Hurin <evgeny.gurin@gmail.com>
2025-08-17 10:23:12 +0300

    Initial

---
+++ neovim.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "cobalt2",
+		},
+	},
+	{
+		"lalitmee/cobalt2.nvim",
+		event = { "ColorSchemePre" }, -- if you want to lazy load
+		dependencies = { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" },
+		init = function()
+			require("colorbuddy").colorscheme("cobalt2")
+		end,
+	},
+}
